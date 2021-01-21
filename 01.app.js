ba1df31786021516ed6d47f03e5b81fce75b38f4
doej96 <doej96@naver.com>
2021-01-21 15:57:46 +0900

    router 분리

--- 01.app.js
+++ 01.app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const moment = require('moment');
 const { mysql, connection } = require('./modules/mysql-conn');
 
+const cityRouter = require('./routes/city-route')
+
 const app = express();
 app.listen(3000, () => {console.log('//127.0.0.1:3000')});
 
@@ -11,19 +13,7 @@ app.set('veiws', path.join(__dirname, 'views'));
 app.locals.pretty = true;
 
 app.use('/', express.static(path.join(__dirname, 'public')));
-
-//도시 리스트
-app.get('/city', (req, res) => {
-  const pug = {
-    file: 'city',
-  }
-  res.render('city/list', pug);
-})
-
-//도시 등록
-app.get('/city/create', (req, res) => {
-  res.send('/city/create');
-})
+app.use('/city', cityRouter);
 
 app.use((req, res) => {
   res.send('/404');
