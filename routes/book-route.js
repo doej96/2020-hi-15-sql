35c4c58255e5b68003205e3c0c2ceae53af2f582
doej96 <doej96@naver.com>
2021-01-25 17:08:58 +0900

    ...

--- routes/book-route.js
+++ routes/book-route.js
@@ -2,10 +2,20 @@ const express = require('express')
 const router = express.Router();
 const { pool } = require('../modules/mysql-pool')
 const { err } = require('../modules/util')
+const moment = require('moment')
 const pugs = {file : 'book', title: '도서등록시스템'}
 
-router.get('/', (req, res, next) => {
-  res.send('리스트')
+router.get('/', async (req, res, next) => {
+  try {
+    const sql = 'SELECT * FROM books ORDER BY id DESC'
+    const r = await pool.query(sql)
+    for(let v of r[0]) v.wdate = moment(v.wdate).format('YYYY-MM-DD');
+    const pug = { ...pugs, r: r[0] }
+    res.render('book/list', pug)
+  }
+  catch(e) {
+    next(err(e.message));
+  }
 })
 
 router.get('/create', (req, res, next) => {
@@ -15,7 +25,7 @@ router.get('/create', (req, res, next) => {
 
 router.post('/save', async (req, res, next) => {
   try {
-    const sql = 'INSERT INTO book SET title=?, writer=?, wdate=?'
+    const sql = 'INSERT INTO books SET title=?, writer=?, wdate=?'
     const value = [req.body.title, req.body.writer, new Date()]
     const r = await pool.query(sql, value);
     res.redirect('/book')
